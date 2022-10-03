ada1bf3429c1934053b578d078ef00a311ed43e1
Kwoolly <104534318+Kwoolly@users.noreply.github.com>
2022-10-02 21:16:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = list(map(int, input('Enter 5 numbers in ascending order').split()))
+# print (numbers)
+insval = int(input('Enter the insertion value'))
+
+i = 0
+while ( i < len(numbers)):
+	if (insval < numbers[i]):
+		numbers.insert(i, insval)
+		break
+	i += 1
+else:
+	numbers.append(insval)
+
+print (numbers)
+
+
